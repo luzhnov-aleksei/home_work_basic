6bc0e4a0ec54ff4f225df51a874b4377a2d948f9
Алексей Лужнов <goshanskiya@gmail.com>
2024-05-08 22:01:04 +0300

    hw03 done

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
+	width := 5
+	height := 5
+	for j := 0; j < height; j++ {
+		for i := 0; i < width; i++ {
+			if j%2 == 0 {
+				fmt.Print(" #")
+			} else {
+				fmt.Print("# ")
+			}
+		}
+		fmt.Print("\n")
+	}
 
 }
