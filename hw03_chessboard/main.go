9e2f2f15bc40be6cc7d6c5bead92f5166685f26e
Алексей Лужнов <goshanskiya@gmail.com>
2024-05-08 22:21:10 +0300

    add input hw03

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -5,8 +5,11 @@ import (
 )
 
 func main() {
-	width := 5
-	height := 5
+	width,height := 0, 0
+	fmt.Print("Введите ширину поля: ")
+	fmt.Scanln(&width)
+	fmt.Print("Введите высоту поля: ")
+	fmt.Scanln(&height)
 	for j := 0; j < height; j++ {
 		for i := 0; i < width; i++ {
 			if j%2 == 0 {
