02839c03b334378be21ae5b521653e833bf367f6
Алексей Лужнов <goshanskiya@gmail.com>
2024-05-09 13:04:20 +0300

    fix_err_again

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,5 +1,20 @@
 package main
 
 func main() {
+	width, height := 0, 0
+	fmt.Print("Введите ширину поля: ")
+	fmt.Scanln(&width)
+	fmt.Print("Введите высоту поля: ")
+	fmt.Scanln(&height)
+	for j := 0; j < height; j++ {
+		for i := 0; i < width; i++ {
+			if j%2 == 0 {
+				fmt.Print(" #")
+			} else {
+				fmt.Print("# ")
+			}
+		}
+		fmt.Print("\n")
+	}
 
 }
