8077717623640fe2bf15b9ea79baf698fc8e8176
Алексей Лужнов <goshanskiya@gmail.com>
2024-05-10 11:04:14 +0300

    fix_errors2

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,13 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	width, height := 0, 0
 	fmt.Print("Введите ширину поля: ")
-	fmt.Scanln(&width)
+	_, err := fmt.Scanln(&width)
+	if err != nil {
+		fmt.Println("Ошибка ввода ширины поля:", err)
+		os.Exit(1)
+	}
 	fmt.Print("Введите высоту поля: ")
-	fmt.Scanln(&height)
+	_, err = fmt.Scanln(&height)
+	if err != nil {
+		fmt.Println("Ошибка ввода высоты поля:", err)
+		os.Exit(1)
+	}
 	for j := 0; j < height; j++ {
 		for i := 0; i < width; i++ {
 			if j%2 == 0 {
@@ -16,6 +27,6 @@ func main() {
 				fmt.Print("# ")
 			}
 		}
-		fmt.Print("\n")
+		fmt.Println()
 	}
 }
