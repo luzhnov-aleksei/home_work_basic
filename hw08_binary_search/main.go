3a80576d46b2e44c9c1b16d27417b50a01f6e9ac
Алексей Лужнов <goshanskiya@gmail.com>
2024-06-16 17:37:00 +0300

    done

--- hw08_binary_search/main.go
+++ hw08_binary_search/main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+)
+
+func binarySearch(arr []int, target int) (int, error) {
+	left := 0
+	right := len(arr) - 1
+	if target < arr[left] || target > arr[right] {
+		return 0, errors.New("значение вне диапазона данных")
+	}
+	for left <= right {
+		mid := left + (right-left)/2
+		if arr[mid] == target {
+			return mid, nil
+		}
+		if target < arr[mid] {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+	return 0, errors.New("значение не найдено")
+}
+
 func main() {
-	// Place your code here.
+	arr := []int{2, 3, 4, 5, 6, 7, 9, 10, 12}
+	a, err := binarySearch(arr, 11)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("Найдено значение %d по индексу %d \n", arr[a], a)
+	}
 }
