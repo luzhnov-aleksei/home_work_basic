a2c34ee90aba21d4ac28ff860e92ab32723098aa
Алексей Лужнов <goshanskiya@gmail.com>
2024-06-16 16:17:44 +0300

    done

--- hw06_testing/chessboard/chessboard.go
+++ hw06_testing/chessboard/chessboard.go
@@ -2,27 +2,25 @@ package chessboard
 
 import (
 	"errors"
-	"fmt"
+	"strings"
 )
 
-func ChessBoard(width, height int) error {
-	if width <= 0 {
-		return errors.New("некорректное значение ширины поля, введите положительное целое число")
-	}
-	if height <= 0 {
-		return errors.New("некорректное значение высоты поля, введите положительное целое число")
+
+func ChessBoard(width, height int) (string, error) {
+	if width <= 0 || height <= 0 {
+		return "", errors.New("ширина и высота должны быть больше нуля")
 	}
 
-	for j := 0; j < height; j++ {
-		for i := 0; i < width; i++ {
-			if j%2 == 0 {
-				fmt.Print(" #")
+	var board strings.Builder
+	for i := 0; i < height; i++ {
+		for j := 0; j < width; j++ {
+			if (i+j)%2 == 0 {
+				board.WriteRune('#') 
 			} else {
-				fmt.Print("# ")
+				board.WriteRune(' ') 
 			}
 		}
-		fmt.Println()
+		board.WriteString("\n")
 	}
-	fmt.Println()
-	return nil
+	return board.String(), nil
 }
--- hw06_testing/chessboard/chessboard_test.go
+++ hw06_testing/chessboard/chessboard_test.go
@@ -1,6 +1,7 @@
 package chessboard
 
 import (
+	"strings"
 	"testing"
 )
 
@@ -8,20 +9,44 @@ func TestChessBoard(t *testing.T) {
 	tests := []struct {
 		width       int
 		height      int
+		expected    string
 		expectError bool
 	}{
-		{8, 8, false},
-		{0, 8, true},
-		{8, 6, false},
-		{-1, 8, true},
-		{8, -1, true},
-		{0, 0, true},
+		{8, 8, strings.TrimSpace(`
+# # # # 
+ # # # #
+# # # #
+ # # # #
+# # # #
+ # # # #
+# # # #
+ # # # #
+		`), false},
+		{0, 8, "", true},
+		{8, 6, strings.TrimSpace(`
+# # # # 
+ # # # #
+# # # #
+ # # # #
+# # # #
+ # # # #
+		`), false},
+		{-1, 8, "", true},
+		{8, -1, "", true},
+		{0, 0, "", true},
 	}
 
 	for _, tt := range tests {
-		err := ChessBoard(tt.width, tt.height)
+		_, err := ChessBoard(tt.width, tt.height)
+
 		if (err != nil) != tt.expectError {
-			t.Errorf("ChessBoard(%d, %d) error = %v, expectError %v", tt.width, tt.height, err, tt.expectError)
+			t.Errorf("ChessBoard(%d, %d) ошибка = %v, ожидалась ошибка %v", tt.width, tt.height, err, tt.expectError)
+			continue
 		}
+
+		if err != nil {
+			continue
+		}
+
 	}
 }
