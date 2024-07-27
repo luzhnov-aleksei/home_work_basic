bb3dcc539d797075a5cc12efe072580b8fc8ac61
luzhnov-aleksei <luzhnov_aleksei@mail.ru>
2024-07-27 11:20:22 +0300

    return old solution

--- hw10_motion_sensor/main.go
+++ hw10_motion_sensor/main.go
@@ -3,52 +3,61 @@ package main
 import (
 	"crypto/rand"
 	"fmt"
+	"math"
 	"math/big"
 	"time"
 )
 
-func main() {
-	rawDataChan := readSensorData()
-	processedDataChan := make(chan float64)
-
-	go processDataUsingAverage(rawDataChan, processedDataChan)
-
-	for data := range processedDataChan {
-		fmt.Printf("Processed data: %.2f\n", data)
-	}
-}
-
-func readSensorData() <-chan int {
-	dataChan := make(chan int)
+func Sensor(max int64, delay time.Duration) chan int {
+	c := make(chan int)
 	go func() {
-		defer close(dataChan)
-		for start := time.Now(); time.Since(start) < time.Minute; {
-			n, err := rand.Int(rand.Reader, big.NewInt(100))
-			if err != nil {
-				fmt.Println("Random number generation error:", err)
+		defer close(c)
+		timeout := time.After(delay)
+		for {
+			select {
+			case <-timeout:
 				return
+			default:
+				r, err := rand.Int(rand.Reader, big.NewInt(max))
+				if err != nil {
+					return
+				}
+				select {
+				case c <- int(r.Int64()):
+				case <-timeout:
+					return
+				}
 			}
-			dataChan <- int(n.Int64())
-			time.Sleep(time.Second)
 		}
 	}()
-	return dataChan
+	return c
 }
 
-func processDataUsingAverage(rawDataChan <-chan int, processedDataChan chan<- float64) {
-	defer close(processedDataChan)
-
-	var sum int
-	var count int
-
-	for data := range rawDataChan {
-		sum += data
-		count++
-		if count == 10 {
-			average := float64(sum) / 10
-			processedDataChan <- average
-			sum = 0
-			count = 0
+func Reader(depth int, inputCh chan int) chan int {
+	c := make(chan int)
+	go func() {
+		defer close(c)
+		counter := 0
+		storage := 0
+		for a := range inputCh {
+			storage += a
+			counter++
+			if counter == depth {
+				mid := math.Ceil(float64(storage) / float64(depth))
+				c <- int(mid)
+				counter = 0
+				storage = 0
+			}
 		}
+	}()
+	return c
+}
+
+func main() {
+	emulationSensor := Sensor(1000, time.Minute)
+	readData := Reader(10, emulationSensor)
+
+	for output := range readData {
+		fmt.Println("readData", output)
 	}
 }
